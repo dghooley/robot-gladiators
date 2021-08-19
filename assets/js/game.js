4eb932d337f4e1887c3eda606a2ec608067b8105
Derick Hooley <dghooley@gmail.com>
2021-08-19 09:01:03 -0500

    for loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,19 +4,19 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 // You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+// console.log(playerName, playerAttack, playerHealth);
 
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames.length);
-for(var i = 0; i < 3; i++) {
-    console.log("apple", i);
-}
-
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]
+/*for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + "is at" + i + "index");
+}*/
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     
@@ -73,4 +73,6 @@ var fight = function() {
 };
 
 // run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
